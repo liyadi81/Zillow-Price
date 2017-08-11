fa8770da4296eb4a17b95cde0b9c0318b69ee9cd
Yadi Li <liyadi@gmail.com>
2017-08-11 13:31:27 -0400

    initiate preprocessing code

---
+++ preprocessing.R
@@ -0,0 +1,87 @@
+
+
+#############impurt librarys##################
+
+library(data.table)
+library(dplyr)
+library(ggplot2)
+library(stringr)
+
+
+############# Importing Data #################
+properties <- fread('properties_2016.csv', na.strings = "")
+train <- fread('train_2016_v2.csv', na.strings = "")
+
+############# rename columns #################
+properties <- properties %>% rename(
+  id_parcel = parcelid,
+  build_year = yearbuilt,
+  area_basement = basementsqft,
+  area_patio = yardbuildingsqft17,
+  area_shed = yardbuildingsqft26,
+  area_pool = poolsizesum,  
+  area_lot = lotsizesquarefeet,
+  area_garage = garagetotalsqft,
+  area_firstfloor_finished = finishedfloor1squarefeet,
+  area_total_calc = calculatedfinishedsquarefeet,
+  area_base = finishedsquarefeet6,
+  area_live_finished = finishedsquarefeet12,
+  area_liveperi_finished = finishedsquarefeet13,
+  area_total_finished = finishedsquarefeet15,  
+  area_unknown = finishedsquarefeet50,
+  num_unit = unitcnt,
+  num_story = numberofstories,  
+  num_room = roomcnt,
+  num_bathroom = bathroomcnt,
+  num_bedroom = bedroomcnt,
+  num_bathroom_calc = calculatedbathnbr,
+  num_bath = fullbathcnt,  
+  num_75_bath = threequarterbathnbr,
+  num_fireplace = fireplacecnt,
+  num_pool = poolcnt,  
+  num_garage = garagecarcnt,  
+  region_county = regionidcounty,
+  region_city = regionidcity,
+  region_zip = regionidzip,
+  region_neighbor = regionidneighborhood,  
+  tax_total = taxvaluedollarcnt,
+  tax_building = structuretaxvaluedollarcnt,
+  tax_land = landtaxvaluedollarcnt,
+  tax_property = taxamount,
+  tax_year = assessmentyear,
+  tax_delinquency = taxdelinquencyflag,
+  tax_delinquency_year = taxdelinquencyyear,
+  zoning_property = propertyzoningdesc,
+  zoning_landuse = propertylandusetypeid,
+  zoning_landuse_county = propertycountylandusecode,
+  flag_fireplace = fireplaceflag,
+  flag_tub = hashottuborspa,
+  quality = buildingqualitytypeid,
+  framing = buildingclasstypeid,
+  material = typeconstructiontypeid,
+  deck = decktypeid,
+  story = storytypeid,
+  heating = heatingorsystemtypeid,
+  aircon = airconditioningtypeid,
+  architectural_style= architecturalstyletypeid
+)
+
+train <- train %>% rename(
+  id_parcel = parcelid)
+
+
+############ this is the raw training data without preprocessing ###############
+train_data <- train %>% 
+  mutate(year=year(transactiondate),
+         month=month(transactiondate)) %>%
+  select(-transactiondate) %>%
+  inner_join(properties, by="id_parcel") 
+
+
+############ droping column and imputation ########################
+col_droping = c() ## adding columns to drop here
+
+
+
+
+
